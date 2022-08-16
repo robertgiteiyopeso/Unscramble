6322b8f2b66dec7cbb197b4d26413fdbb8d3371d
Robert Gitei <robert.gitei@yopeso.com>
2022-08-16 18:40:29 +0300

    Add a ViewModel

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -21,6 +21,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.android.unscramble.R
 import com.example.android.unscramble.databinding.GameFragmentBinding
 
@@ -32,6 +33,8 @@ class GameFragment : Fragment() {
     private var score = 0
     private var currentWordCount = 0
     private var currentScrambledWord = "test"
+
+    private val viewModel: GameViewModel by viewModels()
 
 
     // Binding object instance with access to the views in the game_fragment.xml layout
