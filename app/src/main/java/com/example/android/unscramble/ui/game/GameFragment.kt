d29f90e58da756ece2944532fe275af2cfcc2bb5
Robert Gitei <robert.gitei@yopeso.com>
2022-08-16 19:22:31 +0300

    Update game restart logic

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -113,6 +113,7 @@ class GameFragment : Fragment() {
      * restart the game.
      */
     private fun restartGame() {
+        viewModel.reinitializeData()
         setErrorTextField(false)
         updateNextWordOnScreen()
     }
